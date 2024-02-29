c1e28852c8643688ee346a7b200cd1eba0b9838a
scrhartley <scrhartley@github.com>
2024-02-29 20:37:44 +0000

    Sync fix for overriding button's default styling

--- src/test/java/simon/email/freemarker/ButtonDirectiveTests.java
+++ src/test/java/simon/email/freemarker/ButtonDirectiveTests.java
@@ -26,7 +26,21 @@ public class ButtonDirectiveTests {
     void renderButtonComponentWithPassingValuesFromStyle() {
         String actualOutput = render("<@Button href=\"https://example.com\" style={ \"padding\": \"12px 20px\"} />");
         assertEqualsIgnoringWhitespace("""
-                <a href="https://example.com" style="padding:12px 20px 12px 20px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%" target="_blank"><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%;mso-text-raise:18" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px"></span><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
+                <a href="https://example.com" style="line-height:100%;text-decoration:none;display:inline-block;max-width:100%;padding:12px 20px 12px 20px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%;mso-text-raise:18" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9px"></span><span><!--[if mso]><i style="letter-spacing: 20px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
+                actualOutput);
+    }
+
+    @Test
+    void renderButtonPassingStyleOverridingDefaultValues() {
+        String actualOutput = render("""
+            <@Button href="https://example.com" style={
+                "line-height": "150%",
+                "display": "block",
+                "text-decoration": "underline red",
+                "max-width": "50%"
+            } />""");
+        assertEqualsIgnoringWhitespace("""
+                <a href="https://example.com" style="line-height:150%;text-decoration:underline red;display:block;max-width:50%;padding:0px 0px 0px 0px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 0px;mso-font-width:-100%;mso-text-raise:0" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:0px"></span><span><!--[if mso]><i style="letter-spacing: 0px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
                 actualOutput);
     }
 
@@ -42,7 +56,7 @@ public class ButtonDirectiveTests {
     void renderButtonComponentWithLargeNumbersForPadding() { // Avoid accidental i18n
         String actualOutput = render("<@Button style={ 'padding': '12000px 20000px' } href=\"https://example.com\" />");
         assertEqualsIgnoringWhitespace("""
-                <a href="https://example.com" style="padding:12000px 20000px 12000px 20000px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%" target="_blank"><span><!--[if mso]><i style="letter-spacing: 20000px;mso-font-width:-100%;mso-text-raise:18000" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9000px"></span><span><!--[if mso]><i style="letter-spacing: 20000px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
+                <a href="https://example.com" style="line-height:100%;text-decoration:none;display:inline-block;max-width:100%;padding:12000px 20000px 12000px 20000px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 20000px;mso-font-width:-100%;mso-text-raise:18000" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:9000px"></span><span><!--[if mso]><i style="letter-spacing: 20000px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
                 actualOutput);
     }
 
@@ -58,7 +72,7 @@ public class ButtonDirectiveTests {
     void renderButtonComponentWithPaddingAndLeftPadding() {
         String actualOutput = render("<@Button style={ 'padding': '50px', 'padding-left': 0 } href=\"https://example.com\" />");
         assertEqualsIgnoringWhitespace("""
-                <a href="https://example.com" style="padding:50px 50px 50px 0px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%" target="_blank"><span><!--[if mso]><i style="letter-spacing: 0px;mso-font-width:-100%;mso-text-raise:75" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:37.5px"></span><span><!--[if mso]><i style="letter-spacing: 50px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
+                <a href="https://example.com" style="line-height:100%;text-decoration:none;display:inline-block;max-width:100%;padding:50px 50px 50px 0px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 0px;mso-font-width:-100%;mso-text-raise:75" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:37.5px"></span><span><!--[if mso]><i style="letter-spacing: 50px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
                 actualOutput);
     }
 
@@ -66,7 +80,7 @@ public class ButtonDirectiveTests {
     void renderButtonComponentWithMultiplePaddingUnits() {
         String actualOutput = render("<@Button style={ 'padding': '50% 50px 2em 1.5rem' } href=\"https://example.com\" />");
         assertEqualsIgnoringWhitespace("""
-                <a href="https://example.com" style="padding:300px 50px 32px 24px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%" target="_blank"><span><!--[if mso]><i style="letter-spacing: 24px;mso-font-width:-100%;mso-text-raise:249" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:24px"></span><span><!--[if mso]><i style="letter-spacing: 50px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
+                <a href="https://example.com" style="line-height:100%;text-decoration:none;display:inline-block;max-width:100%;padding:300px 50px 32px 24px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 24px;mso-font-width:-100%;mso-text-raise:249" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:24px"></span><span><!--[if mso]><i style="letter-spacing: 50px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
                 actualOutput);
     }
 
@@ -74,7 +88,7 @@ public class ButtonDirectiveTests {
     void renderButtonComponentWithUnsupportedUnitsAndNumbers() {
         String actualOutput = render("<@Button style={ 'padding': '1.5rem 13cm', 'padding-left': 2 } href=\"https://example.com\" />");
         assertEqualsIgnoringWhitespace("""
-                <a href="https://example.com" style="padding:24px 0px 24px 2px;line-height:100%;text-decoration:none;display:inline-block;max-width:100%" target="_blank"><span><!--[if mso]><i style="letter-spacing: 2px;mso-font-width:-100%;mso-text-raise:36" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:18px"></span><span><!--[if mso]><i style="letter-spacing: 0px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
+                <a href="https://example.com" style="line-height:100%;text-decoration:none;display:inline-block;max-width:100%;padding:24px 0px 24px 2px" target="_blank"><span><!--[if mso]><i style="letter-spacing: 2px;mso-font-width:-100%;mso-text-raise:36" hidden>&nbsp;</i><![endif]--></span><span style="max-width:100%;display:inline-block;line-height:120%;mso-padding-alt:0px;mso-text-raise:18px"></span><span><!--[if mso]><i style="letter-spacing: 0px;mso-font-width:-100%" hidden>&nbsp;</i><![endif]--></span></a>""",
                 actualOutput);
     }
 
